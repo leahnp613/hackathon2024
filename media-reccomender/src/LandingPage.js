3561a5b00705ab258a37a89fbb0749dbdef09794
leahnp613 <leahnagarpowers@gmail.com>
2024-02-25 12:31:27 -0500

    landing page and app.js import

---
+++ media-reccomender/src/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Music Mood Quiz!</h1>
+            <p>Find the perfect music for your current mood.</p>
+            {/* Add more content here */}
+        </div>
+    );
+}
+
+export default LandingPage;
